25f00afc964d3dd51d873e8d0f334cfd4b33c771
oficial-rilwan <aribidesirilwan1477@gmail.com>
2024-09-11 11:52:43 +0100

    feature updates

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,7 @@
 "use strict";
 console.log("Hello World");
+setYear();
+function setYear() {
+  const el = document.getElementById("year");
+  if (el) el.textContent = String(new Date().getFullYear());
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+setYear();
+
+function setYear() {
+  const el = document.getElementById("year");
+  if (el) el.textContent = String(new Date().getFullYear());
+}
